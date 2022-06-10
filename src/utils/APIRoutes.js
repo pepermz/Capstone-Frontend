cb772d5448304cad95d0e71ed3d0c10454df7666
pepermz <jose.ram.18@gmail.com>
2022-06-10 16:48:34 -0600

    yeah

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
 
-export const host = "https://agile-cliffs-81775.herokuapp.com/";
+export const host = "https://salty-lake-84804.herokuapp.com";
 export const registerRoute = `${host}/api/auth/register`
 export const loginRoute = `${host}/api/auth/login`
 export const setAvatarRoute = `${host}/api/auth/setAvatar`
